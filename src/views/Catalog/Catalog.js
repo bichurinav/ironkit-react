ec7b15b320a77ddb9d84d5b7ef77920f766267be
bichurinav <bichurinet@ya.ru>
2021-04-04 21:28:18 +0300

    add Catalog

---
+++ src/views/Catalog/Catalog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Catalog.scss';
+
+export default function Catalog() {
+    return (
+        <div className="catalog">
+            <div className="catalog__inner">
+                <div className="catalog__filter"></div>
+                <div className="catalog__content">card</div>
+            </div>
+        </div>
+    );
+}
